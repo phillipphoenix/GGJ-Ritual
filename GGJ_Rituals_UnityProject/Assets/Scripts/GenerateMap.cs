1c679979c2b9bcbf3fbeeb84127ebda5b28cc199
Phillip Phoelich <pphoelich@gmail.com>
2016-01-30 03:30:33 +0100

    Perlin noise tree placement added. We can still tweak values and all, but it's in!

---
+++ GGJ_Rituals_UnityProject/Assets/Scripts/GenerateMap.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateMap : MonoBehaviour
+{
+
+    public Vector2 MapOrigin;
+    public Vector2 MapSize;
+
+    // Perlin Noise and placement.
+    public int Seed;
+    public int FractionBrownianMotionIterations;
+    public float MinDensityForTrees;
+    public GameObject TreePrefab;
+    public int SkipSpaces;
+
+    public void Awake()
+    {
+        for (int y = 0; y < MapSize.y; y += SkipSpaces)
+        {
+            for (int x = 0; x < MapSize.x; x += SkipSpaces)
+            {
+                float density = FractionBrownianMotion(FractionBrownianMotionIterations, x / MapSize.x, y / MapSize.y);
+                //Debug.Log("(" + x + "," + y +"), " + density + " > " + MinDensityForTrees);
+                if (density > MinDensityForTrees)
+                {
+                    GameObject tree = GameObject.Instantiate(TreePrefab);
+                    tree.transform.position = new Vector3(x + MapOrigin.x, 0, y + MapOrigin.y);
+                }
+            }
+        }
+    }
+
+    private float FractionBrownianMotion(int iterations, float x, float y)
+    {
+
+        float value = 0;
+        for (int i = 1; i <= iterations; ++i)
+        {
+            // TODO Instead of "iterations" inside the PerlinNoise function, use "i".
+            value += Mathf.PerlinNoise(Seed + x * iterations, Seed + y * iterations) / iterations;
+        }
+        return value;
+    }
+}

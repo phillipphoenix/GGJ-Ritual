46b3e44d62f8f51f4d3cc3b874692750d3974419
Phillip Phoelich <pphoelich@gmail.com>
2016-01-30 16:07:56 +0100

    Generation of totems and villages (positions) is now done. Actual generation of each totems looks and the village's houses and peoples are not in this implementation.

--- GGJ_Rituals_UnityProject/Assets/Scripts/GenerateMap.cs
+++ GGJ_Rituals_UnityProject/Assets/Scripts/GenerateMap.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class GenerateMap : MonoBehaviour
 {
@@ -23,15 +25,30 @@ public class GenerateMap : MonoBehaviour
     // Values for placing totems.
     [Header("Totem settings")]
     public int NbOfTotems;
+    public GameObject TotemPrefab;
+    public float MinTotemDistance;
+    public float TotemClearingDistance;
+    private List<Transform> totemList;
+
+    // Values for placing villages.
+    [Header("Village settings")]
+    public int NbOfVillages;
+    public GameObject VillagePrefab;
+    public float MinVillageDistance;
+    public float VillageClearingDistance;
+    private List<Transform> villageList; 
 
     public void Awake()
     {
         Random.seed = Seed;
         GenerateTrees();
+        GenerateTotems();
+        GenerateVillages();
     }
 
     private void GenerateTrees()
     {
+        GameObject TreeParent = new GameObject("Trees");
         for (int y = 0; y < MapSize.y; y += SkipSpaces)
         {
             for (int x = 0; x < MapSize.x; x += SkipSpaces)
@@ -42,8 +59,80 @@ public class GenerateMap : MonoBehaviour
                     GameObject tree = GameObject.Instantiate(TreePrefab);
                     Vector2 offset = new Vector2(Random.value * MaxOffset.x, Random.value * MaxOffset.y);
                     tree.transform.position = new Vector3(x + MapOrigin.x + offset.x, 0, y + MapOrigin.y + offset.y);
+                    tree.transform.parent = TreeParent.transform;
                 }
             }
+        }
+    }
+
+    private void GenerateTotems()
+    {
+        totemList = new List<Transform>();
+        GameObject totemParent = new GameObject("Totems");
+        for (int i = 0; i < NbOfTotems; i++)
+        {
+            var x = Random.value * MapSize.x;
+            var y = Random.value * MapSize.y;
+            if (DistanceToNearestTransform(new Vector3(x, 0, y), totemList) < MinTotemDistance)
+            {
+                x = Random.value * MapSize.x;
+                y = Random.value * MapSize.y;
+                Debug.Log("Totem too close to other totem.");
+            }
+            GameObject totem = GameObject.Instantiate(TotemPrefab);
+            totem.transform.position = new Vector3(x + MapOrigin.x, 0, y + MapOrigin.y);
+            totem.transform.parent = totemParent.transform;
+            totemList.Add(totem.transform);
+            ClearLandscapeObstacles(totem.transform, TotemClearingDistance);
+        }
+    }
+
+    private void GenerateVillages()
+    {
+        villageList = new List<Transform>();
+        GameObject villageParent = new GameObject("Villages");
+        for (int i = 0; i < NbOfVillages; i++)
+        {
+            var x = Random.value * MapSize.x;
+            var y = Random.value * MapSize.y;
+            while (DistanceToNearestTransform(new Vector3(x, 0, y), villageList.Concat(totemList).ToList()) < MinVillageDistance)
+            {
+                x = Random.value * MapSize.x;
+                y = Random.value * MapSize.y;
+                Debug.Log("Village too close to either other village or to totem.");
+            }
+            GameObject village = GameObject.Instantiate(VillagePrefab);
+            village.transform.position = new Vector3(x + MapOrigin.x, 0, y + MapOrigin.y);
+            village.transform.parent = villageParent.transform;
+            villageList.Add(village.transform);
+            ClearLandscapeObstacles(village.transform, VillageClearingDistance);
+        }
+    }
+
+    private float DistanceToNearestTransform(Vector3 currentPos, List<Transform> transformList)
+    {
+        float minDist = Mathf.Infinity;
+        foreach (Transform t in transformList)
+        {
+            float dist = Vector3.Distance(t.position, currentPos);
+            if (dist < minDist)
+            {
+                minDist = dist;
+            }
+        }
+        return minDist;
+    }
+
+    private void ClearLandscapeObstacles(Transform objTransform, float distance)
+    {
+        var transformArray = GameObject.FindGameObjectsWithTag("LandscapeObstacle")
+         .Select(go => go.transform)
+         .Where(t => Vector3.Distance(t.position, objTransform.position) < TotemClearingDistance)
+         .ToArray();
+        foreach (var obstacle in transformArray)
+        {
+            Debug.Log("Clearing landscape obstacle!");
+            GameObject.Destroy(obstacle.gameObject);
         }
     }
 
@@ -64,5 +153,11 @@ public class GenerateMap : MonoBehaviour
             }
         }
         return value;
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, new Vector3(MapSize.x, 10, MapSize.y));
     }
 }

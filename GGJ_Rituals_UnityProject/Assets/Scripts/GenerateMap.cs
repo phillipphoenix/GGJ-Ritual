350494d4d6fd1ff3f72f311375d4c9e70ae524cf
Phillip Phoelich <pphoelich@gmail.com>
2016-01-30 14:38:36 +0100

    Updated the tree placement again.

--- GGJ_Rituals_UnityProject/Assets/Scripts/GenerateMap.cs
+++ GGJ_Rituals_UnityProject/Assets/Scripts/GenerateMap.cs
@@ -7,29 +7,36 @@ public class GenerateMap : MonoBehaviour
     public Vector2 MapOrigin;
     public Vector2 MapSize;
 
-    // Perlin Noise and placement.
+    // General values.
     public int Seed;
     public int FractionBrownianMotionIterations;
+
+    // Values for generating trees.
     public float MinDensityForTrees;
     public GameObject TreePrefab;
     public int SkipSpaces;
+    public bool UseIterationsForTrees;
 
     // Random offset values.
     public Vector2 MaxOffset;
 
     public void Awake()
     {
+        Random.seed = Seed;
+        GenerateTrees();
+    }
+
+    private void GenerateTrees()
+    {
         for (int y = 0; y < MapSize.y; y += SkipSpaces)
         {
             for (int x = 0; x < MapSize.x; x += SkipSpaces)
             {
                 float density = FractionBrownianMotion(FractionBrownianMotionIterations, x / MapSize.x, y / MapSize.y);
-                //Debug.Log("(" + x + "," + y +"), " + density + " > " + MinDensityForTrees);
                 if (density > MinDensityForTrees)
                 {
                     GameObject tree = GameObject.Instantiate(TreePrefab);
                     Vector2 offset = new Vector2(Random.value * MaxOffset.x, Random.value * MaxOffset.y);
-                    Debug.Log(offset);
                     tree.transform.position = new Vector3(x + MapOrigin.x + offset.x, 0, y + MapOrigin.y + offset.y);
                 }
             }
@@ -38,12 +45,19 @@ public class GenerateMap : MonoBehaviour
 
     private float FractionBrownianMotion(int iterations, float x, float y)
     {
-
         float value = 0;
         for (int i = 1; i <= iterations; ++i)
         {
-            // TODO Instead of "iterations" inside the PerlinNoise function, use "i".
-            value += Mathf.PerlinNoise(Seed + x * iterations, Seed + y * iterations) / iterations;
+            if (UseIterationsForTrees)
+            {
+                // TODO Instead of "iterations" inside the PerlinNoise function, use "i".
+                value += Mathf.PerlinNoise(Seed + x * iterations, Seed + y * iterations) / iterations;
+            }
+            else
+            {
+                // TODO Instead of "iterations" inside the PerlinNoise function, use "i".
+                value += Mathf.PerlinNoise(Seed + x * i, Seed + y * i) / iterations;
+            }
         }
         return value;
     }
